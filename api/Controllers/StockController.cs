5db4cd66a2427a60085e6997d829135bf1d35ade
Dennis Vo <dennisphuc@gmail.com>
2024-06-19 16:53:38 +0700

    feat: finish DTOs and Mappers for Stock without Comments
    src: 5. DTOs and Mappers

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Data;
+using api.Mappers;
 using Microsoft.AspNetCore.Mvc;
 
 namespace api.Controllers
@@ -19,7 +20,8 @@ namespace api.Controllers
 
         [HttpGet]
         public IActionResult GetAll(){
-            var stock = _context.Stocks.ToList();
+            var stock = _context.Stocks.ToList()
+            .Select(s => s.ToStockDto());
 
             return Ok(stock);
         }
@@ -33,7 +35,7 @@ namespace api.Controllers
                 return NotFound();
             }
 
-            return Ok(stock);
+            return Ok(stock.ToStockDto());
         }
     }
 }
