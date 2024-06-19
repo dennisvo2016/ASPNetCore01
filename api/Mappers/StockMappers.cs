45ce8527d1025292190c9e90263a28200dd918b2
Dennis Vo <dennisphuc@gmail.com>
2024-06-20 00:02:58 +0700

    feat: finish HTTP POST
    src: 6. POST request

--- api/Mappers/StockMappers.cs
+++ api/Mappers/StockMappers.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using api.Dtos.Stock;
 using api.Models;
@@ -19,6 +20,17 @@ namespace api.Mappers
                 LastDiv = stockModel.LastDiv,
                 Industry = stockModel.Industry,
                 MarketCap = stockModel.MarketCap
+            };
+        }
+
+        public static Stock ToStockFromCreateDTO(this CreateStockRequestDto stockDto){
+            return new Stock{
+                Symbol = stockDto.Symbol,
+                CompanyName = stockDto.CompanyName,
+                Purchase = stockDto.Purchase,
+                LastDiv = stockDto.LastDiv,
+                Industry = stockDto.Industry,
+                MarketCap = stockDto.MarketCap
             };
         }
     }
