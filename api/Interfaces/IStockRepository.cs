8a6dde9f26de51fde157ceb76e1f905779c58705
Dennis Vo <dennisphuc@gmail.com>
2024-07-03 16:36:42 +0700

    feat: finish refactoring Controller and Repository, move all db logic to inside Repository class
    src: video 11

--- api/Interfaces/IStockRepository.cs
+++ api/Interfaces/IStockRepository.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using api.Dtos.Stock;
 using api.Models;
 
 namespace api.Interfaces
@@ -9,6 +10,9 @@ namespace api.Interfaces
     public interface IStockRepository
     {
         Task<List<Stock>> GetAllAsync();
-        
+        Task<Stock?> GetByIdAsync(int id); //Using ? because firstOrDefault can return null
+        Task<Stock> CreateAsync(Stock stockModel);
+        Task<Stock?> UpdateAsync(int id, UpdateStockRequestDto stockDto);//StockDto from the request
+        Task<Stock?> DeleteAsync(int id); //Can return null when not found the item
     }
 }
