981466711b39e7924990eae6944fe5d8dff9ff6d
Dennis Vo <dennisphuc@gmail.com>
2024-07-10 15:10:51 +0200

    feat: finish Account controller and register endpoint with database setup
    src: video 22

---
+++ api/Migrations/20240710124507_SeedRole.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "68e66706-e762-4cbd-a1b6-5ba247ca0893", null, "User", "USER" },
+                    { "857a43f5-eeaf-41be-93ab-248ea8dfec56", null, "Admin", "ADMIN" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "68e66706-e762-4cbd-a1b6-5ba247ca0893");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "857a43f5-eeaf-41be-93ab-248ea8dfec56");
+        }
+    }
+}
