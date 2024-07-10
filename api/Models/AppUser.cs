a6d7922e0fc351e6ae8235774bfc32aed0015d6d
Dennis Vo <dennisphuc@gmail.com>
2024-07-10 11:25:46 +0200

    feat: install and configure MS Identity and JWT Auth
    src: video 21

---
+++ api/Models/AppUser.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace api.Models
+{
+    public class AppUser : IdentityUser
+    {
+        //IdentityUser by default provide email, password and also other extension if needed in the future
+    }
+}
